cec3003342b6aeab914b8bbfe656ab503b5cc562
Tom Overland <tomoverland03@gmail.com>
2020-11-25 13:59:10 -0600

    created SearchYelp method on App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,6 +26,10 @@ const businesses = [
 ];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log("Searching Yelp for " + {term} + " near " + {location} + " using " + {sortBy});
+  }
+
   render() {
     return (
       <div className="App">
