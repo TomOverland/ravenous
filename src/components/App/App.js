e819ca4d982e5a952d4ae45ba2e811b8cc7aec48
Tom Overland <tomoverland03@gmail.com>
2020-12-01 13:24:01 -0600

    removed hard coded business vars in App, corrected query params on Yelp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import './App.css';
-
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Bordertown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
+import Yelp from '../../util/Yelp'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses = []
+    }
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp for ${term} near ${location} by ${sortBy}`);
+    // console.log(`Searching Yelp for ${term} near ${location} by ${sortBy}`);
+    Yelp.search().then(businesses => {
+      this.setState({
+        businesses: businesses
+      })
+    })
   }
 
   render() {
@@ -42,3 +34,11 @@ class App extends React.Component {
 }
 
 export default App;
+
+// Ideas to improve application:
+// Make addresses clickable and have them open the address in Google Maps in a new tab
+// Make images clickable and have them open the business’ website in a new tab
+// Clicking on a different sorting option automatically requeries the Yelp API, rather than having to manually click “Let’s Go” again
+// Implement your own type of sort (for example, by entering a distance or radius from a central location)
+// Allow you to search by pressing “Enter” (or “Return”) on your keyboard, as opposed to manually clicking
+// Add autocompletion of addresses to the “Location” input
