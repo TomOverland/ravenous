875a3f2b9f32f511319814a7e3408bf69d3ea263
Tom Overland <tomoverland03@gmail.com>
2020-12-07 09:23:48 -0600

    Updates

---
+++ precache-manifest.ae0da7bb1c0e07d238569b77c4870ed3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27e6d14dad957a1d58bbd91f9cc00ec",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "d35b76c465669ebc33cb",
+    "url": "/ravenous/static/css/main.ecfbe5b8.chunk.css"
+  },
+  {
+    "revision": "ccea9fd76ec54ba3ff13",
+    "url": "/ravenous/static/js/2.3053200f.chunk.js"
+  },
+  {
+    "revision": "d35b76c465669ebc33cb",
+    "url": "/ravenous/static/js/main.08c39fc5.chunk.js"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
